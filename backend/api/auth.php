c0369ec667ecda3289aac2975d7164ab7c2e9cc3
OscarF2834 <pipesegoviaalvarado@gmail.com>
2025-10-29 17:18:42 -0500

    cambios

--- backend/api/auth.php
+++ backend/api/auth.php
@@ -1,5 +1,15 @@
 <?php
 // backend/api/auth.php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+
 header('Content-Type: application/json');
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../helpers/jwt_helper.php';
